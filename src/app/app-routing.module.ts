7e24ff3650f1292b109709c060531066ae199c87
Jack D <dmarkov23@gmail.com>
2018-03-24 18:51:30 +0100

    setup of child routes for personal-posts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,25 @@ import {HomeComponent} from './components/home/home.component';
 import {RegisterComponent} from './components/register/register.component';
 import {LoginComponent} from './components/login/login.component';
 
+import {ListComponent} from './components/personal-posts/list/list.component';
+import {CreatePostComponent} from './components/personal-posts/create-post/create-post.component';
+import {HomeComponent as HomeComponentPersonalPosts} from './components/personal-posts/home/home.component';
+
 
 const routes: Routes = [
   {path: '', redirectTo: '/home', pathMatch: 'full'},
   {path: 'home', component: HomeComponent},
   {path: 'register', component: RegisterComponent},
   {path: 'login', component: LoginComponent},
-  {path: 'create-posts', component: RegisterComponent}
+  {
+    path: 'personal-posts', component: HomeComponentPersonalPosts, children: [
+      {path: '', redirectTo: '/list', pathMatch: 'full'},
+      {path: 'list', component: ListComponent},
+      {path: 'create', component: CreatePostComponent}
+    ]
+  },
+  {path: 'personal-posts/create', component: CreatePostComponent}
+
 
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,14 @@ import {NavBarComponent} from './components/nav-bar/nav-bar.component';
 import {LoginComponent} from './components/login/login.component';
 import {RegisterComponent} from './components/register/register.component';
 import {PostComponent} from './components/post/post.component';
-import {CreatePostComponent} from './components/create-post/create-post.component';
 import {HomeComponent} from './components/home/home.component';
 import {AppRoutingModule} from './/app-routing.module';
 import {UserService} from './services/user.service';
 import {HttpClientModule} from '@angular/common/http';
 import {PostService} from './services/post.service';
 import {FormsModule} from '@angular/forms';
+import {ListComponent} from './components/personal-posts/list/list.component';
+import {HomeComponent as HomeComponentPersonalPosts} from './components/personal-posts/home/home.component';
 
 // import {ReactiveFormsModule} from '@angular/forms';
 
@@ -24,9 +25,10 @@ import {FormsModule} from '@angular/forms';
     LoginComponent,
     RegisterComponent,
     PostComponent,
-    CreatePostComponent,
     HomeComponent,
-    // ReactiveFormsModule
+    ListComponent,
+    HomeComponentPersonalPosts
+
   ],
   imports: [
     BrowserModule,
